ff64594965b6a05f6a907447c5437b2294b7d8ac
DuyNguyen <elearning.nkd@gmail.com>
2023-10-12 16:32:26 +0700

    use ModelAndView

--- demo/src/main/java/com/codegym/demo/controller/CustomerController.java
+++ demo/src/main/java/com/codegym/demo/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class CustomerController {
 
     CustomerService customerService = CustomerService.getInstance();
 
+//    C1
 //    @GetMapping("/customers")
 //    public String showList(HttpServletRequest request) {
 //        List<Customer> customers = customerService.findAll();
@@ -21,12 +23,23 @@ public class CustomerController {
 //        return "customers/list";
 //    }
 
+//    C2
+//    @GetMapping("/customers")
+//    public String showList(Model model) {
+//        List<Customer> customers = customerService.findAll();
+//        model.addAttribute("customers", customers);
+//        return "customers/list";
+//    }
+
+//    C3
     @GetMapping("/customers")
-    public String showList(Model model) {
+    public ModelAndView showList(){
+        ModelAndView modelAndView = new ModelAndView("customers/list");
         List<Customer> customers = customerService.findAll();
-        model.addAttribute("customers", customers);
-        return "customers/list";
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
     }
+
     @GetMapping("/customer")
     public String showCustomer(@RequestParam int id, Model model) {
         Customer customer = customerService.findById(id);
